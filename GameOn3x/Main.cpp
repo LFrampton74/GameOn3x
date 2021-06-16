949ec805b42fe2aa2e079e4f72ba94ba82715c76
gypsy <gypsy@DESKTOP-IQ0NDQ9>
2021-06-15 22:14:31 -0400

    Add project files.

---
+++ GameOn3x/Main.cpp
@@ -0,0 +1,12 @@
+// This include allows us to output to the screen. Wk1-1
+#include <iostream>
+// Right now this keeps us from typing std in front of many lines of code. Wk1-1
+using namespace std;
+// This is the entry point for C++.  Wk1-1
+int main()
+{
+	// This prints out to the display.  Wk1-1
+	cout << "Game On!" << endl;
+	// This ends our program.  Wk1-1
+	return 0;
+}
